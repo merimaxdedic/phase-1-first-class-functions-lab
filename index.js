2059648944a1555ed71756611e568decfcff0581
Merima Dedic <merimaxdedic@gmail.com>
2023-11-07 19:28:15 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(driversArray) {
+    return driversArray.slice(0, 2);
+  }
+
+  function returnLastTwoDrivers(driversArray) {
+    return driversArray.slice(-2);
+  }
+  const selectingDrivers = [returnFirstTwoDrivers];
+selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }
+
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+  
+  function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+  }
